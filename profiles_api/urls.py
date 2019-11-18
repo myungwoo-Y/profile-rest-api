8ffc8d184f0918dfd3cdfd37084ebd80323b420b
myungwoo-Y <auddn6676@naver.com>
2019-11-18 13:13:28 +0900

    Added viewset to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+# 기본 경로를 이용하기 위해 사용
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+# router를 이용하여 손 쉽게 url 설정 가능
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
