a3a93e62180f27bcca8696801010e01c10ce78bd
myungwoo-Y <auddn6676@naver.com>
2019-11-18 11:47:24 +0900

    practice http method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+# http status를 나타낸다.
+from rest_framework import status
+
+from profiles_api import serializers
+
 class HelloApiView(APIView):
     """Test API View"""
     
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Returns a list ofAPIView freatures"""
         an_apiview = [
@@ -13,4 +20,33 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message' : 'Hello!', 'an_apiview' : an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        # 들어오는 값을 알맞게 변화시킨다.
+        # input 값을 validate 한다.
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message' : message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method' :'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method' :'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method' :'DELETE'})
+
+        
 
